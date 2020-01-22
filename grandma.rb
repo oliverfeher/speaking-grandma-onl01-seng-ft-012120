2c9d29216747e77b5242887a92a7fd39526de8c8
Oliver Feher <olcsee@gmail.com>
2020-01-22 06:50:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,6 +15,9 @@
 def speak_to_grandma(string)
   if string == string.upcase
     puts "NO, NOT SINCE 1938!"
+  elseif string == "I LOVE YOU GRANDMA"
+    puts "I LOVE YOU TOO PUMPKIN!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
+  end
 end
