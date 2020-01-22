3414695a796b015f6be422c79c135826da95a35b
Oliver Feher <olcsee@gmail.com>
2020-01-22 06:46:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-  if string == string.upcase
+  if string != string.upcase
     puts "NO, NOT SINCE 1938"
   elseif string == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
